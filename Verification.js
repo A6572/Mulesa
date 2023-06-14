97a47b25ab9a1b137e7d029e464c2cbc97fb4836
A6572 <102286171+A6572@users.noreply.github.com>
2023-06-14 12:57:29 +0300

    Add files via upload

--- Verification.js
+++ Verification.js
@@ -1,38 +1,46 @@
-//Admin verification page
-  document.addEventListener("DOMContentLoaded", function () {
-    const iframe = document.querySelector("#content-iframe");
-    const btns = document.querySelectorAll(".btn");
-    btns.forEach(function (btn) {
-      btn.addEventListener("click", function () {
-        const src = this.dataset.src;
-        iframe.src = src;
-      });
-    });
-
-    const adminName = prompt("Please enter your Administration Username:");
-    document.getElementById("admin-name").textContent = adminName;
-
-    function verifyPassword() {
-      const password = prompt("Welcome " + adminName + ". Please enter your password:");
-      const validPassword = "123"; // Change this to your desired password
-
-      if (password === validPassword) {
-        document.getElementById("admin-password").textContent = password;
-      } else {
-        alert("Access denied! Invalid password.");
-        window.location.href = "Verification.html"; // Redirect to the login page or any other URL
-      }
-    }
-
-    // Time
-    setInterval(function () {
-      const now = new Date();
-      const dateTimeString = now.toLocaleString();
-      document.getElementById("date-time").textContent = dateTimeString;
-    }, 1000);
-
-    verifyPassword(); // Call the password verification function
-  });
+   // Admin verification page
+     document.addEventListener("DOMContentLoaded", function() {
+       const iframe = document.querySelector("#content-iframe");
+       const btns = document.querySelectorAll(".btn");
+       btns.forEach(function(btn) {
+         btn.addEventListener("click", function() {
+           const src = this.dataset.src;
+           iframe.src = src;
+         });
+       });
+  
+       const adminCredentials = [
+         { name: "Mary", password: "100" },
+         { name: "Joseph", password: "200" },
+           // Add more admin names and passwords as needed
+         ];
+  
+       function verifyAdmin() {
+         const adminName = prompt(" Administration Username:");
+         const adminPassword = prompt("Hello " + adminName +". Please enter your password:");
+  
+         const admin = adminCredentials.find(function(admin) {
+           return admin.name === adminName && admin.password === adminPassword;
+         });
+  
+         if (admin) {
+           document.getElementById("admin-name").textContent = adminName;
+           document.getElementById("admin-email").textContent = adminName;
+         } else {
+           alert("Access denied! Invalid credentials.");
+           window.location.href = "Verification.html"; // Redirect to the login page or any other URL
+         }
+       }
+  
+       // Time
+       setInterval(function() {
+         const now = new Date();
+         const dateTimeString = now.toLocaleString();
+         document.getElementById("date-time").textContent = dateTimeString;
+       }, 1000);
+  
+       verifyAdmin(); // Call the admin verification function
+     });
   
     // Replace the current page's URL with a new URL
     function closePreviousPages() {
