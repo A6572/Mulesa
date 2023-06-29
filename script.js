0ad8d3acf57511a386924c64599cc79c846f7c26
A6572 <102286171+A6572@users.noreply.github.com>
2023-06-29 18:12:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,3 +40,28 @@
     function closePopup() {
       document.getElementById("popup").style.display = "none";
     }
+    //users
+    var users = [
+  { username: "Admin", password: "admin", email: "antonio@200labs.tech", phoneNumber: "+254111111" },
+  { username: "Mary", password: "100", email: "marym@gmail.com", phoneNumber: "079234566" },
+  { username: "Joseph", password: "200", email: "jkitonga@gmail.com", phoneNumber: "0794562649" }
+];
+
+       // Time
+       setInterval(function() {
+         const now = new Date();
+         const dateTimeString = now.toLocaleString();
+         document.getElementById("date-time").textContent = dateTimeString;
+       }, 1000);
+  
+       verifyAdmin(); // Call the admin verification function
+  
+    // Replace the current page's URL with a new URL
+    function closePreviousPages() {
+     if (typeof history.pushState === "function") {
+        history.pushState(null, "", location.href);
+        window.addEventListener("popstate", function () {
+          location.href = "index.html";
+        });
+      }
+    }
